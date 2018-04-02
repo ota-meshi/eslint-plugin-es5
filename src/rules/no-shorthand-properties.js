72d5b3df9043bd9a5129a1f4126bbadbbd9aeddd
yosuke ota <otameshiyo23@gmail.com>
2018-04-03 00:07:50 +0900

    [update] Make `es5/no-shorthand-properties` fixable
    This Commit makes `es5/no-shorthand-properties` fixable.
    The autofix transforms
    `var foo = { bar }` to `var foo = { bar: bar }`
    `var foo = { bar() {} }` to `var foo = { bar: function() {} }`.

--- src/rules/no-shorthand-properties.js
+++ src/rules/no-shorthand-properties.js
@@ -5,15 +5,28 @@ module.exports = {
     docs: {
       description: 'Forbid shorthand properties'
     },
+    fixable: 'code',
     schema: []
   },
   create(context) {
+    const sourceCode = context.getSourceCode()
     return {
       Property(node) {
         if (node.shorthand || node.method) {
           context.report({
             node,
-            message: 'Unexpected object shorthand property.'
+            message: 'Unexpected object shorthand property.',
+            fix: node.shorthand ?
+              fixer => fixer.insertTextAfter(node.key, `: ${node.key.name}`) :
+              node.value.generator ? 
+              fixer => {
+                const asterisk = sourceCode.getTokens(node).find(token => token.type === 'Punctuator' && token.value === "*")
+                const text = sourceCode.text
+                const keyText = text.slice(node.range[0], asterisk.range[0]) + text.slice(asterisk.range[1], node.value.range[0])
+                const replacement = `${keyText}: ${node.value.async ? 'async ' : ''}function*`
+                return fixer.replaceTextRange([node.range[0], node.value.range[0]], replacement)
+              } :
+              fixer => fixer.insertTextBefore(node.value, `: ${node.value.async ? 'async ' : ''}function`)
           });
         }
       }
--- tests/rules/no-shorthand-properties.js
+++ tests/rules/no-shorthand-properties.js
@@ -6,7 +6,30 @@ module.exports = {
     'var foo = { bar: function () {} };'
   ],
   invalid: [
-    { code: 'var foo = { bar };', errors: [{ message: 'Unexpected object shorthand property.' }] },
-    { code: 'var foo = { bar() {} };', errors: [{ message: 'Unexpected object shorthand property.' }] }
+    {
+      code: 'var foo = { bar };',
+      output: 'var foo = { bar: bar };',
+      errors: [{ message: 'Unexpected object shorthand property.' }]
+    },
+    {
+      code: 'var foo = { bar() {} };',
+      output: 'var foo = { bar: function() {} };',
+      errors: [{ message: 'Unexpected object shorthand property.' }]
+    },
+    {
+      code: 'var foo = { bar(a) {} };',
+      output: 'var foo = { bar: function(a) {} };',
+      errors: [{ message: 'Unexpected object shorthand property.' }]
+    },
+    {
+      code: 'var foo = { *[bar]() {} };',
+      output: 'var foo = { [bar]: function*() {} };',
+      errors: [{ message: 'Unexpected object shorthand property.' }]
+    },
+    {
+      code: 'var foo = { /*1*/ * /*2*/ [ /*3*/ bar /*4*/ ] /*5*/ () /*6*/ { /*7*/ } /*8*/ };',
+      output: 'var foo = { /*1*/  /*2*/ [ /*3*/ bar /*4*/ ] /*5*/ : function*() /*6*/ { /*7*/ } /*8*/ };',
+      errors: [{ message: 'Unexpected object shorthand property.' }]
+    }
   ]
 };
